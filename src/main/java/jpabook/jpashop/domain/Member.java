50812de635f4522edcfefa9acbca06cf4143fef2
seunghwanLee <dkqlch@naver.com>
2022-05-19 23:25:33 +0900

    상속관계 매핑

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -14,6 +14,7 @@ public class Member extends BaseEntity{
     private String city;
     private String street;
     private String zipcode;
+    private String sex;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
